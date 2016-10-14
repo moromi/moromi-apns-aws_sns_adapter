2a95a06ad55861fda1fde830e392acc6459dc812
Takahiro Ooishi <taka0125@gmail.com>
2016-10-14 13:34:55 +0900

    :recycle: Refactoring

--- lib/moromi/apns/aws_sns_adapter/aws_sns_extension.rb
+++ lib/moromi/apns/aws_sns_adapter/aws_sns_extension.rb
@@ -1,9 +1,9 @@
 class Moromi::Aws::Sns::Client
-  def send_apns_message(arn:, message:, sandbox: true)
-    send_apns(arn: arn, params: message.to_aws_sns_params, sandbox: sandbox)
+  def send_apns_message(arn, message)
+    send_apns(arn, message.to_aws_sns_params)
   end
 
-  def send_apns_message_to_topic(topic_arn:, message:, sandbox: true)
-    send_apns_to_topic(topic_arn: topic_arn, params: message.to_aws_sns_params, sandbox: sandbox)
+  def send_apns_message_to_topic(topic_arn, message)
+    send_apns_to_topic(topic_arn, message.to_aws_sns_params)
   end
 end
