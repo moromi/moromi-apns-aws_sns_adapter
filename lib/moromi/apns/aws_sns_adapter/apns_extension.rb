43aca1193d21ccfb102012f7ea72cd155d4fdc13
Takahiro Ooishi <taka0125@gmail.com>
2017-09-01 18:51:44 +0900

    :+1: fix

--- lib/moromi/apns/aws_sns_adapter/apns_extension.rb
+++ lib/moromi/apns/aws_sns_adapter/apns_extension.rb
@@ -2,17 +2,7 @@ class Moromi::Apns::Message::Base
   include Moromi::Aws::Sns::Message::Base
 
   def to_parameter
-    base = {
-      aps: {
-        badge: badge,
-        alert: alert,
-        sound: sound,
-        'content-available' => content_available,
-        'mutable-content' => mutable_content,
-        priority: priority
-      }
-    }
-    Moromi::Aws::Sns::Message::Parameter.new(apns: custom_data.merge(base))
+    Moromi::Aws::Sns::Message::Parameter.new(apns: to_hash)
   end
 end
 
--- moromi-apns-aws_sns_adapter.gemspec
+++ moromi-apns-aws_sns_adapter.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'activesupport', ['>= 4.2']
-  spec.add_dependency 'moromi-apns', '< 1.0'
+  spec.add_dependency 'moromi-apns', '< 1.0', '~> 0.7'
   spec.add_dependency 'moromi-aws-sns', '< 1.0', '~> 0.5'
 
   spec.add_development_dependency "bundler", "~> 1.12"
